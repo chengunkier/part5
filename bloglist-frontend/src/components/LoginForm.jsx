92489380774e68e4aa41269bf86cab7926a0448c
chengunkier <inabamicheal97@gmail.com>
2026-06-25 11:55:27 +0300

    completed exercise 5.1: blog list frontend, step1

---
+++ bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,34 @@
+const LoginForm = ({
+  username,
+  password,
+  handleUsernameChange,
+  handlePasswordChange,
+  handleSubmit
+}) => (
+  <div>
+    <h2>log in to application</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+)
+
+export default LoginForm
