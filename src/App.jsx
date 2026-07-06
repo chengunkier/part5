56b6b8d288510d9a0d03b5cbe7031387e2e4d137
chengunkier <inabamicheal97@gmail.com>
2026-07-06 14:56:44 +0300

    completed exercise 5.10 blog list frontend, step10

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
       const returnedBlog = await blogService.update(updatedBlog.id, updatedBlog)
       setBlogs(blogs.map(blog =>
         blog.id === returnedBlog.id
-          ? { ...returnedBlog, user: blog.user }  // keep original populated user so name stays visible
+          ? { ...returnedBlog, user: blog.user }
           : blog
       ))
     } catch (exception) {
@@ -120,9 +120,12 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-      )}
+      {[...blogs]
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} handleLike={handleLike} />
+        )
+      }
     </div>
   )
 }
