b1cd3435ff60479df3e7aec9e4197280e5b19edd
chengunkier <inabamicheal97@gmail.com>
2026-07-07 10:25:09 +0300

    completed exercise 5.14: blog list tests, step2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 const blog = {
@@ -20,7 +21,7 @@ const mockUser = {
 test('renders title and author but not url or likes by default', () => {
   render(<Blog blog={blog} handleLike={() => {}} handleDelete={() => {}} user={mockUser} />)
 
-  // title should be visible
+  // title and author should be visible
   expect(screen.getByText('Component testing is done with react-testing-library Test Author')).toBeDefined()
 
   // url should NOT be visible by default
@@ -28,4 +29,23 @@ test('renders title and author but not url or likes by default', () => {
 
   // likes should NOT be visible by default
   expect(screen.queryByText('likes 5')).toBeNull()
+})
+
+test('url, likes and user are shown when view button is clicked', async () => {
+  render(<Blog blog={blog} handleLike={() => {}} handleDelete={() => {}} user={mockUser} />)
+
+  const user = userEvent.setup()
+
+  // click the view button
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  // url should now be visible
+  expect(screen.getByText('https://example.com')).toBeDefined()
+
+  // likes should now be visible
+  expect(screen.getByText('likes 5')).toBeDefined()
+
+  // user name should now be visible
+  expect(screen.getByText('Test User')).toBeDefined()
 })
