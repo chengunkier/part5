61ff24d38124889b90d7e9b0069d33a0afa62d98
chengunkier <inabamicheal97@gmail.com>
2026-07-17 10:47:43 +0300

    completed exercise 5.29: styled blogs, step 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate, useParams } from 'react-router-dom'
 import Notification from './components/Notification'
 import SingleBlog from './components/SingleBlog'
+import CreateBlog from './components/CreateBlog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -22,11 +23,11 @@ const Navigation = ({ user, handleLogout }) => {
 
 const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
   return (
-    <div>
+    <div className="login-form">
       <h2>Log in to application</h2>
       <form onSubmit={handleLogin}>
-        <div>
-          username
+        <div className="form-group">
+          <label>username</label>
           <input
             type="text"
             value={username}
@@ -34,8 +35,8 @@ const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }
             onChange={({ target }) => setUsername(target.value)}
           />
         </div>
-        <div>
-          password
+        <div className="form-group">
+          <label>password</label>
           <input
             type="password"
             value={password}
@@ -65,56 +66,6 @@ const BlogList = ({ blogs }) => {
           )
         }
       </ul>
-    </div>
-  )
-}
-
-const CreateBlog = ({ createBlog }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    createBlog({ title, author, url })
-    setTitle('')
-    setAuthor('')
-    setUrl('')
-  }
-
-  return (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          title:
-          <input
-            type="text"
-            value={title}
-            name="Title"
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            type="text"
-            value={author}
-            name="Author"
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            type="text"
-            value={url}
-            name="Url"
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
     </div>
   )
 }
--- src/components/CreateBlog.jsx
+++ src/components/CreateBlog.jsx
@@ -14,33 +14,36 @@ const CreateBlog = ({ createBlog }) => {
   }
 
   return (
-    <div>
+    <div className="create-form">
       <h2>create new</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          title:
+        <div className="form-group">
+          <label>title:</label>
           <input
             type="text"
             value={title}
             name="Title"
+            placeholder="title"
             onChange={({ target }) => setTitle(target.value)}
           />
         </div>
-        <div>
-          author:
+        <div className="form-group">
+          <label>author:</label>
           <input
             type="text"
             value={author}
             name="Author"
+            placeholder="author"
             onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
-        <div>
-          url:
+        <div className="form-group">
+          <label>url:</label>
           <input
             type="text"
             value={url}
             name="Url"
+            placeholder="url"
             onChange={({ target }) => setUrl(target.value)}
           />
         </div>
